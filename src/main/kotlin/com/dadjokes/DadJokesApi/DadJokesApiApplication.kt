ab17d97f5635ebf15130686221d5ea4de0731287
Anthony Shannon <ashannon1990@gmail.com>
2024-01-15 11:04:16 -0700

    initial commit

---
+++ src/main/kotlin/com/dadjokes/DadJokesApi/DadJokesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.dadjokes.DadJokesApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DadJokesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DadJokesApiApplication>(*args)
+}
